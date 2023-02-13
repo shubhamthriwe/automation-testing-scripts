3b3ee0221913e1eaa2d1ec3e98901ab9f77aae73
Shubhamthriwe <shubham.sisodia@thriwe.com>
2023-02-13 12:31:13 +0530

    Bug Fixes

--- Automation Testing/Payment_test_script.py
+++ Automation Testing/Payment_test_script.py
@@ -1,15 +1,231 @@
 import requests
 import time
+import json
 import unittest
 import testMethods as external
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 class TestPaymentCheckout(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.maximize_window()
  
-    def test_successful_payment(self):
+    def test_case_1(self):
+        test_case = 1
+        # Payment request api call
+        url = "http://localhost:8088/v1/payments"
+        # payload={}
+
+        tokenPayload = external.JwtPayload(
+            issuer = "FAB_BENEFITS_V1",
+            amount = 1200,
+            currency = "INR",
+            email = "shubham.sisodia@thriwe.com",
+            contactNumber = "9012100577",
+            referenceId = "45dd",
+            description = "Amazon 250 voucher booking",
+            preAuth = True,
+            frontendReturnUrl = "https://thriwe.com/index.html",
+            backendReturnUrl = "https://eogtmtsbqyib8xf.m.pipedream.net",
+            cards = [
+                {
+                    "first" :"411111",
+                    "last" :"1111"
+                }
+            ],
+            binNumbers = [
+                "411111"
+            ],
+            pgCode = "PG02"
+            )
+        
+        headers = {
+            'Authorization': 'Bearer ' + external.generateTokenAndSetHeader(tokenPayload)
+        }
+
+        response = requests.request("POST", url, headers=headers, data={})
+        data = response.json()
+        if response.status_code == 200:
+            nextAction = data.get('nextAction')
+            redirectUrl = nextAction.get('redirectUrl')
+
+            # Navigate to the Checkout payment page
+            self.browser.get(redirectUrl)
+            # time.sleep(1) #Slow down the process
+
+            #Submit payment details
+            external.submitCardDetails(self.browser,"5111111111111111","08","2026","345","Shubham Sisodia")
+            
+            # Delay the screen
+            time.sleep(2)
+            #Check if there is any error in make payment call
+            try:
+                err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
+            except:
+                external.handle_page_redirection(self,test_case)   
+            else:
+                if err_msg_field.text != "":
+                    external.logMessage(test_case,"Error",err_msg_field.text)
+                else:
+                    # Wait for 5 seconds until the URL changes
+                    wait = WebDriverWait(self.browser, 5)
+                    try:
+                        element = wait.until(EC.url_changes(redirectUrl))
+                    except TimeoutException:
+                        external.logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+                    else:
+                        # print("Here 1")
+                        external.handle_page_redirection(self,test_case)
+            
+        else:
+            external.logMessage(test_case,"Error"," Make payment error : " + json.dumps(data))
+    
+    def test_case_2(self):
+        test_case = 2
+        # Payment request api call
+        url = "http://localhost:8088/v1/payments"
+        # payload={}
+
+        tokenPayload = external.JwtPayload(
+            issuer = "FAB_BENEFITS_V1",
+            amount = 1200,
+            currency = "INR",
+            email = "shubham.sisodia@thriwe.com",
+            contactNumber = "9012100577",
+            referenceId = "45dd",
+            description = "Amazon 250 voucher booking",
+            preAuth = True,
+            frontendReturnUrl = "https://thriwe.com/index.html",
+            backendReturnUrl = "https://eogtmtsbqyib8xf.m.pipedream.net",
+            cards = [
+                {
+                    "first" :"555555",
+                    "last" :"4444"
+                }
+            ],
+            binNumbers = [
+                "55555"
+            ],
+            pgCode = "PG02"
+            )
+        
+        headers = {
+            'Authorization': 'Bearer ' + external.generateTokenAndSetHeader(tokenPayload)
+        }
+
+        response = requests.request("POST", url, headers=headers, data={})
+        data = response.json()
+        if response.status_code == 200:
+            nextAction = data.get('nextAction')
+            redirectUrl = nextAction.get('redirectUrl')
+
+            # Navigate to the Checkout payment page
+            self.browser.get(redirectUrl)
+            # time.sleep(1) #Slow down the process
+
+            #Submit payment details
+            external.submitCardDetails(self.browser,"5555555555554444","08","2026","345","Shubham Sisodia")
+            
+            # Delay the screen
+            time.sleep(2)
+
+            #Check if there is any error in make payment call
+            try:
+                err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
+            except:
+                external.handle_page_redirection(self,test_case)   
+            else:
+                if err_msg_field.text != "":
+                    external.logMessage(test_case,"Error",err_msg_field.text)
+                else:
+                    # Wait for 5 seconds until the URL changes
+                    wait = WebDriverWait(self.browser, 5)
+                    try:
+                        element = wait.until(EC.url_changes(redirectUrl))
+                    except TimeoutException:
+                        external.logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+                    else:
+                        external.handle_page_redirection(self,test_case)
+        else:
+            external.logMessage(test_case,"Error"," Make payment error : " + json.dumps(data))
+    
+    def test_case_3(self):
+        test_case = 3
+        # Payment request api call
+        url = "http://localhost:8088/v1/payments"
+        # payload={}
+
+        tokenPayload = external.JwtPayload(
+            issuer = "FAB_BENEFITS_V1",
+            amount = 1200,
+            currency = "INR",
+            email = "shubham.sisodia@thriwe.com",
+            contactNumber = "9012100577",
+            referenceId = "45dd",
+            description = "Amazon 250 voucher booking",
+            preAuth = True,
+            frontendReturnUrl = "https://thriwe.com/index.html",
+            backendReturnUrl = "https://eogtmtsbqyib8xf.m.pipedream.net",
+            cards = [
+                {
+                    "first" :"400000",
+                    "last" :"3220"
+                }
+            ],
+            binNumbers = [
+                "400000"
+            ],
+            pgCode = "PG01"
+            )
+        
+        headers = {
+            'Authorization': 'Bearer ' + external.generateTokenAndSetHeader(tokenPayload)
+        }
+
+        response = requests.request("POST", url, headers=headers, data={})
+        data = response.json()
+        if response.status_code == 200:
+            nextAction = data.get('nextAction')
+            redirectUrl = nextAction.get('redirectUrl')
+
+            # Navigate to the Checkout payment page
+            self.browser.get(redirectUrl)
+            # time.sleep(1) #Slow down the process
+
+            #Submit payment details
+            external.submitCardDetails(self.browser,"4000000000003220","08","2026","345","Shubham Sisodia")
+            
+            # Delay the screen
+            time.sleep(2)
+
+            #Check if there is any error in make payment call
+            try:
+                err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
+            except:
+                external.handle_page_redirection(self,test_case)   
+            else:
+                if err_msg_field.text != "":
+                    external.logMessage(test_case,"Error",err_msg_field.text)
+                else:
+                    # Wait for 5 seconds until the URL changes
+                    wait = WebDriverWait(self.browser, 5)
+                    try:
+                        element = wait.until(EC.url_changes(redirectUrl))
+                    except TimeoutException:
+                        external.logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+                    else:
+                        # print("Here 1")
+                        external.handle_page_redirection(self,test_case)
+        else:
+            external.logMessage(test_case,"Error"," Make payment error : " + json.dumps(data))
+    
+    def test_case_4(self):
+        test_case = 4
         # Payment request api call
         url = "http://localhost:8088/v1/payments"
         # payload={}
@@ -32,7 +248,7 @@ class TestPaymentCheckout(unittest.TestCase):
                 }
             ],
             binNumbers = [
-                "424242","434343"
+                "424242"
             ],
             pgCode = "PG01"
             )
@@ -49,7 +265,7 @@ class TestPaymentCheckout(unittest.TestCase):
 
             # Navigate to the Checkout payment page
             self.browser.get(redirectUrl)
-            time.sleep(1) #Slow down the process
+            # time.sleep(1) #Slow down the process
 
             #Submit payment details
             external.submitCardDetails(self.browser,"4242424242424242","08","2026","345","Shubham Sisodia")
@@ -58,25 +274,107 @@ class TestPaymentCheckout(unittest.TestCase):
             time.sleep(2)
 
             #Check if there is any error in make payment call
-            err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
-            if err_msg_field.text != "":
-                external.logMessage(err_msg_field.text)
+            try:
+                err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
+            except:
+                external.handle_page_redirection(self,test_case)   
             else:
-                #success case
-                time.sleep(3)
-                # Get the title of the webpage
-                print("Payment Status page : " + self.browser.title)
-                time.sleep(2)
-                print("Redirected to frontend url : " + self.browser.title)
-
-
-
+                if err_msg_field.text != "":
+                    external.logMessage(test_case,"Error",err_msg_field.text)
+                else:
+                    # Wait for 5 seconds until the URL changes
+                    wait = WebDriverWait(self.browser, 5)
+                    try:
+                        element = wait.until(EC.url_changes(redirectUrl))
+                    except TimeoutException:
+                        external.logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+                    else:
+                        # print("Here 1")
+                        external.handle_page_redirection(self,test_case)
         else:
-            external.logMessage(data)
- 
+            external.logMessage(test_case,"Error"," Make payment error : " + json.dumps(data))
+    
+    def test_case_5(self):
+        test_case = 5
+        # Payment request api call
+        url = "http://localhost:8088/v1/payments"
+        # payload={}
+
+        tokenPayload = external.JwtPayload(
+            issuer = "FAB_BENEFITS_V",
+            amount = 1200,
+            currency = "INR",
+            email = "shubham.sisodia@thriwe.com",
+            contactNumber = "9012100577",
+            referenceId = "45dd",
+            description = "Amazon 250 voucher booking",
+            preAuth = True,
+            frontendReturnUrl = "https://thriwe.com/index.html",
+            backendReturnUrl = "https://eogtmtsbqyib8xf.m.pipedream.net",
+            cards = [
+                {
+                    "first" :"424242",
+                    "last" :"4242"
+                }
+            ],
+            binNumbers = [
+                "424242"
+            ],
+            pgCode = "PG01"
+            )
+        
+        headers = {
+            'Authorization': 'Bearer ' + external.generateTokenAndSetHeader(tokenPayload)
+        }
+
+        response = requests.request("POST", url, headers=headers, data={})
+        data = response.json()
+        if response.status_code == 200:
+            nextAction = data.get('nextAction')
+            redirectUrl = nextAction.get('redirectUrl')
+
+            # Navigate to the Checkout payment page
+            self.browser.get(redirectUrl)
+            # time.sleep(1) #Slow down the process
+
+            #Submit payment details
+            external.submitCardDetails(self.browser,"4242424242424242","08","2026","345","Shubham Sisodia")
+            
+            # Delay the screen
+            time.sleep(2)
+
+            #Check if there is any error in make payment call
+            try:
+                err_msg_field = self.browser.find_element(By.CLASS_NAME,"notify-message")
+            except:
+                external.handle_page_redirection(self,test_case)   
+            else:
+                if err_msg_field.text != "":
+                    external.logMessage(test_case,"Error",err_msg_field.text)
+                else:
+                    # Wait for 5 seconds until the URL changes
+                    wait = WebDriverWait(self.browser, 5)
+                    try:
+                        element = wait.until(EC.url_changes(redirectUrl))
+                    except TimeoutException:
+                        external.logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+                    else:
+                        # print("Here 1")
+                        external.handle_page_redirection(self,test_case)
+        else:
+            external.logMessage(test_case,"Error"," Make payment error : " + json.dumps(data))
+    
     def tearDown(self):
         self.browser.close()
 
 if __name__ == '__main__':
+    # Run all test cases at once
     unittest.main()
+
+    # Run a particular test case separately
+    # test_suite = unittest.TestSuite()
+    # test_suite.addTest(TestPaymentCheckout('test_case_2'))
+    # unittest.TextTestRunner().run(test_suite)
+
+
 
--- Automation Testing/testMethods.py
+++ Automation Testing/testMethods.py
@@ -3,6 +3,9 @@ import jwt
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 class JwtPayload:
     def __init__(self, issuer,amount,currency,email,contactNumber,referenceId,description,preAuth,frontendReturnUrl,backendReturnUrl,cards,binNumbers,pgCode):
@@ -29,53 +32,53 @@ def submitCardDetails(browser,cardNumber, expiryMonth, expiryYear, cvv, cardhold
     # Enter Card Number
     cardNumber_field = browser.find_element(By.NAME,"cardNumber")
     cardNumber_field.send_keys(cardNumber)
-    time.sleep(500/1000) 
+    # time.sleep(500/1000) 
 
     # Enter Expiry Month
     expiryMonth_field = browser.find_element(By.NAME,"expiryMonth")
     expiryMonth_field.send_keys(expiryMonth)
-    time.sleep(500/1000)
+    # time.sleep(500/1000)
 
     # Enter Expiry Year
     expiryYear_field = browser.find_element(By.NAME,"expiryYear")
     expiryYear_field.send_keys(expiryYear)
-    time.sleep(500/1000)
+    # time.sleep(500/1000)
 
     # Enter Cvv Number
     cvvNumber_field = browser.find_element(By.NAME,"cvv")
     cvvNumber_field.send_keys(cvv)
-    time.sleep(500/1000)
+    #time.sleep(500/1000)
 
     # Enter Card Holder Name
     cardHolderName_field = browser.find_element(By.NAME,"customerName")
     cardHolderName_field.send_keys(cardholderName)
-    time.sleep(500/1000)
+    #time.sleep(500/1000)
 
     #Submit
     submitButton = browser.find_element(By.ID,"submitButton")
     submitButton.send_keys(Keys.RETURN)
  
-def logMessage(msg):
+def logMessage(testCase,type,msg):
     print("\n===============================================>\n")
-    print("Error Test Case 1 : Make payment error -> %s" % msg)
+    print("Test Case %d : %s -> %s" % (testCase,type,msg))
     print("\n===============================================>\n")
 
 def generateTokenAndSetHeader(JwtPayload):
     # the payload containing the claims
     payload = {
         "iss": JwtPayload.issuer,
-  "amount": JwtPayload.amount,
-  "currency": JwtPayload.currency,
-  "email": JwtPayload.email,
-  "contactNumber": JwtPayload.contactNumber,
-  "referenceId": JwtPayload.referenceId,
-  "description": JwtPayload.description,
-  "preAuth": JwtPayload.preAuth,
-  "frontendReturnUrl": JwtPayload.frontendReturnUrl,
-  "backendReturnUrl": JwtPayload.backendReturnUrl,
-  "cards": JwtPayload.cards,
-  "binNumbers": JwtPayload.binNumbers,
-  "pgCode": JwtPayload.pgCode
+        "amount": JwtPayload.amount,
+        "currency": JwtPayload.currency,
+        "email": JwtPayload.email,
+        "contactNumber": JwtPayload.contactNumber,
+        "referenceId": JwtPayload.referenceId,
+        "description": JwtPayload.description,
+        "preAuth": JwtPayload.preAuth,
+        "frontendReturnUrl": JwtPayload.frontendReturnUrl,
+        "backendReturnUrl": JwtPayload.backendReturnUrl,
+        "cards": JwtPayload.cards,
+        # "binNumbers": JwtPayload.binNumbers,
+        "pgCode": JwtPayload.pgCode
     }
 
     # the secret key used to sign the JWT
@@ -83,10 +86,76 @@ def generateTokenAndSetHeader(JwtPayload):
 
     # encode the payload with the secret key
     encoded_jwt = jwt.encode(payload, secret_key, algorithm='HS256')
-
-    # convert the encoded JWT to a string
-    # jwt_string = encoded_jwt.decode('utf-8')
-
-    # print the JWT string
-    # print(jwt_string)
     return encoded_jwt
+
+
+def check_browser_redirection(currentUrl,self,test_case):
+    self.browser.maximize_window()
+    wait = WebDriverWait(self.browser, 5)
+    try:
+        # print("Here current url is",self.browser.current_url)
+        element = wait.until(EC.url_changes(currentUrl))
+        
+    except TimeoutException:
+        logMessage(test_case,"Error","The URL did not redirected to payment status page.")
+    else:
+        #test for frontend url
+        wait = WebDriverWait(self.browser, 5)
+        try:
+            element = wait.until(EC.url_changes(self.browser.current_url))
+        except TimeoutException:
+            logMessage(test_case,"Error","Page did not redirected to frontend url page."+" Payment Status : " + self.browser.title)
+        else:
+            logMessage(test_case,"Success","Successfully redirected to frontend url: " + self.browser.current_url)
+
+def handle_page_redirection(self,test_case):
+    # print("current_url: " + self.browser.current_url)
+    if self.browser.current_url.startswith("https://hooks.stripe.com/"):
+        time.sleep(4)
+        #This is Stripe 3-D Secure Payment
+        self.browser.switch_to.default_content()
+        self.browser.switch_to.frame(0)
+
+        newFrame = self.browser.find_element(By.ID,"challengeFrame")
+        self.browser.switch_to.frame(newFrame)
+        submitButton = self.browser.find_element(By.ID,"test-source-authorize-3ds")
+        currentUrl = self.browser.current_url
+        submitButton.send_keys(Keys.RETURN)
+        check_browser_redirection(currentUrl,self,test_case)
+                        
+    elif self.browser.current_url.startswith("https://api.razorpay.com/v1/gateway"):
+        #This is Razorpay Domestic Payment
+        submitButton = self.browser.find_element(By.CLASS_NAME,"success")
+        currentUrl = self.browser.current_url
+        submitButton.send_keys(Keys.RETURN)
+        check_browser_redirection(currentUrl,self,test_case)
+        
+    elif self.browser.current_url.startswith("https://api.razorpay.com/v1/payments"):
+        #This is Razorpay International Payment
+        self.browser.set_window_size(500, 1000)
+
+        submitButton = self.browser.find_element(By.ID,"submit-action")
+        currentUrl = self.browser.current_url
+        submitButton.send_keys(Keys.RETURN)
+
+        wait = WebDriverWait(self.browser, 5)
+        try:
+            element = wait.until(EC.url_changes(currentUrl))
+        except TimeoutException:
+            logMessage(test_case,"Error","The URL did not redirected to razorpay success popup page.")
+        else:
+            submitButton = self.browser.find_element(By.CLASS_NAME,"success")
+            submitButton.send_keys(Keys.RETURN)
+            check_browser_redirection(currentUrl,self,test_case)
+        
+
+    else:
+        #This is a normal Payment
+        #test for frontend url
+        wait = WebDriverWait(self.browser, 5)
+        try:
+            element = wait.until(EC.url_changes(self.browser.current_url))
+        except TimeoutException:
+            logMessage(test_case,"Error","Page did not redirected to frontend url page."+" Payment Status : " + self.browser.title)
+        else:
+            logMessage(test_case,"Success","Successfully redirected to frontend url: " + self.browser.current_url)
